b47a68afb2fb8747f0f1103c48738c9a399f2bc3
Fabio Sosa <fabio_sosapy@outlook.com>
2025-06-26 19:47:01 -0300

    feat (destinationList.ts): add constant

---
+++ src/components/destinations/constants/destinationList.ts
@@ -0,0 +1,28 @@
+import type { Props as DestionationCard } from "../ui/DestinationCard.astro";
+
+export const destinationList: DestionationCard[] = [
+    {
+        photo: '/images/ksamil.webp',
+        place: 'Golden Bridge, Ba Na hill',
+        country: 'Vietnam',
+        url: '/explore/vietname'
+    },
+    {
+        photo: '/images/thailand.webp',
+        place: 'Thailand, surfoest oest',
+        country: 'Cancun',
+        url: '/explore/vietname'
+    },
+    {
+        photo: '/images/ushguli.webp',
+        place: 'Ushguli Bridge, land overcome',
+        country: 'Sarah',
+        url: '/explore/vietname'
+    },
+    {
+        photo: '/images/ksamil.webp',
+        place: 'Golden Bridge, Ba Na hill',
+        country: 'Vietnam',
+        url: '/explore/vietname'
+    },
+]
