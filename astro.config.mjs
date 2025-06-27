a20f6d12e9402c6ab9539e1c20755b53b1b68042
Fabio Sosa <fabio_sosapy@outlook.com>
2025-06-27 11:18:54 -0300

    feat (astro.config): add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
